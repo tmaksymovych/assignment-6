f87d3decef970217328b31e1d1659f27994ebf6e
tmaksymovych <tmaksymovych@kse.org.ua>
2025-07-04 21:47:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 available_stocks = ["AAPL", "TSLA", "GOOGL", "MSFT", "AMZN"]
 stock_name = input(f"Choose one of actions from list: {', '.join(available_stocks)} : ")
-
-if stock_name not in available_stocks:
-    print("Error: Please choose a stock from the list.")
-else:
-    ticker = yf.Ticker(f"{stock_name}")
-    data = ticker.history(period="1y")
-    data = data.round(2)
-
+while True:
+    if stock_name not in available_stocks:
+        print("Error: Please choose a stock from the list.")
+        stock_name = input(f"Choose one of actions from list: {', '.join(available_stocks)} : ")
+    else:
+        ticker = yf.Ticker(f"{stock_name}")
+        data = ticker.history(period="1y")
+        data = data.round(2)
+        break
 data["Sell"] = False
 data["Buy"] = False
 data["Hold"] = False
@@ -20,12 +21,12 @@ def rolling_mean():
     data["long"] = data["Close"].rolling(window=20).mean()
 
     for index in range(20, len(data)):
-        if data["short"].iloc[index] > data["long"].iloc[index] and data["short"].iloc[index-1] < data["long"].iloc[index-1]:
-            data["Buy"].iloc[index] = True
-        elif data["short"].iloc[index] < data["long"].iloc[index] and data["short"].iloc[index-1] > data["long"].iloc[index-1]:
-            data["Sell"].iloc[index] = True
+        if data["short"].iloc[index] > data["long"].iloc[index] and data["short"].iloc[index-1] <= data["long"].iloc[index-1]:
+            data.loc[data.index[index], "Buy"] = True
+        elif data["short"].iloc[index] < data["long"].iloc[index] and data["short"].iloc[index-1] >= data["long"].iloc[index-1]:
+            data.loc[data.index[index], "Sell"] = True
         else:
-            data["Hold"].iloc[index] = True
+            data.loc[data.index[index], "Hold"] = True
 
 def run_bot():
         try:
@@ -35,13 +36,13 @@ def run_bot():
             return
         number_of_stocks = 0
         initial_balance = balance
-        for i in range(len(data) - 1):
-            if data["Buy"].iloc[i] and balance > 0:
-                trade_price = data["Open"].iloc[i+1]
+        for index in range(len(data) - 1):
+            if data["Buy"].iloc[index] and balance > 0:
+                trade_price = data["Open"].iloc[index+1]
                 number_of_stocks = balance / trade_price
                 balance = 0
-            elif data["Sell"].iloc[i] and number_of_stocks > 0:
-                trade_price = data["Open"].iloc[i+1]
+            elif data["Sell"].iloc[index] and number_of_stocks > 0:
+                trade_price = data["Open"].iloc[index+1]
                 balance = number_of_stocks * trade_price
                 number_of_stocks = 0 
 
@@ -52,7 +53,7 @@ def run_bot():
         print(f"Initial balance: {initial_balance}$")
         print(f"Final balance: {final_balance}$")
         profit = round(final_balance - initial_balance,2)
-        print(f"Profi:{profit}$")
+        print(f"Profit: {profit}$")
 
 rolling_mean()
 run_bot()
